0b1fcf101447ec535afd47badb8c13b169866ce9
F53 <fseusb@gmail.com>
2022-10-19 19:44:52 -0600

    begin the hellish relations this project requires

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  has_many :users
+  belongs_to :lobby_leader, class_name: 'User', foreign_key: 'lobby_leader_id'
+  belongs_to :round_leader, class_name: 'User', foreign_key: 'round_leader_id'
+  belongs_to :black_card
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  belongs_to :game
+
   # makes hand an array somehow
   # https://joecastronovo.medium.com/how-to-add-an-array-column-to-sqlite3-table-662037d49b64
   serialize :hand
   after_initialize do |u|
     u.hand = [] if u.hand.nil?
   end
+
+  # TODO: belongs_to hand_cards, class_name: 'WhiteCard', foreign_keys: 'hand'
 end
