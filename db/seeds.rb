21a4501c2d4850fce7abcbe69949e96b103a94d4
F53 <fseusb@gmail.com>
2022-11-02 15:34:21 -0600

    add progress bars to seed delete operations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
+def destroy_all_with_progressbar(thing, text)
+  entries = thing.all
+  progressbar = ProgressBar.create(title: "  #{text}", total: entries.length)
+  entries.each do |entry|
+    entry.destroy
+    progressbar.increment
+  end
+end
+
 puts '🗑️ Clearing database'
-BlackCard.destroy_all
-WhiteCard.destroy_all
-CardPack.destroy_all
-CardCategory.destroy_all
+destroy_all_with_progressbar(BlackCard, 'Black Cards')
+destroy_all_with_progressbar(WhiteCard, 'White Cards')
+destroy_all_with_progressbar(CardPack, 'Card Packs')
+destroy_all_with_progressbar(CardCategory, 'Card Categories')
 
 begin
   puts "\n📁 Reading cards file"
@@ -14,7 +23,9 @@ rescue Errno::ENOENT
 else
   cards_json = JSON.parse(card_file)
 
+  # threads = cards_json.map do |pack|
   cards_json.each do |pack|
+    # Thread.new do
     # create the card pack
     card_pack = CardPack.create(title: pack['name'])
 
@@ -22,7 +33,7 @@ else
     puts "\n⌛ Loading cards from #{pack['name']}"
 
     if pack['white'].present?
-      progressbar = ProgressBar.create(title: "      #{pack['white'].length} White cards", total: pack['white'].length)
+      progressbar = ProgressBar.create(title: "  #{pack['white'].length} White cards", total: pack['white'].length)
 
       pack['white'].each do |card|
         WhiteCard.create(text: card['text'], card_pack: card_pack)
@@ -33,14 +44,20 @@ else
 
     next if pack['black'].blank?
 
-    progressbar = ProgressBar.create(title: "      #{pack['black'].length} Black cards", total: pack['black'].length)
+    progressbar = ProgressBar.create(title: "  #{pack['black'].length} Black cards", total: pack['black'].length)
 
     pack['black'].each do |card|
       BlackCard.create(text: card['text'], card_pack: card_pack, pick: card['pick']) if card['pick'] == 1
       # TODO: implement support for cards with card.pick > 1
       progressbar.increment
     end
+    # end
   end
+
+  # threads.each do |thread, i|
+  #   sleep 0.5 while thread.alive?
+  #   puts "finished thread #{i}"
+  # end
 
   # make the categories
   puts "\n🌳 Making categories"
