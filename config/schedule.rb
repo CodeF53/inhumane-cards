61139ca38835f08648c636476d3ed69b67344717
F53 <fseusb@gmail.com>
2022-11-07 10:45:21 -0700

    redo crontab?

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every '@reboot' do
-  command 'source /home/f53/.rvm/scripts/rvm && cd /home/f53/inhumane-cards && bundle exec rails s -b 0.0.0.0 -p 3000 -e production -d &'
+  command 'sleep 30 && source /home/f53/.rvm/scripts/rvm && cd /home/f53/inhumane-cards && bundle exec rails s -e production'
 end
